ac3b789fa280a5c882a2a09ca04b116fee608e99
Noman Ashraf <73111863+nomanashraf874@users.noreply.github.com>
2022-02-13 16:52:43 -0500

    Add files via upload

---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Noman Ashraf on 2/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
